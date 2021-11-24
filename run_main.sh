a28f45ced5ac6d8c3814b8e6a16d80de4e0001b1
SzymonTurek <szymko149@gmail.com>
2021-11-24 12:36:40 +0100

    Add bowtie mapping function

--- run_main.sh
+++ run_main.sh
@@ -3,8 +3,8 @@
 SAMPLE1=(HI.0640.005.Index_13.B_R1 HI.0640.005.Index_14.C_R1)
 SAMPLE2=(HI.0640.005.Index_13.B_R2 HI.0640.005.Index_14.C_R2)
 SAMPLES="HI.0640.005.Index_13.B_R1 HI.0640.005.Index_13.B_R2 HI.0640.005.Index_14.C_R1 HI.0640.005.Index_14.C_R2"
-#SAMPLES_NAMES=(HI.0640.005.Index_13.B HI.0640.005.Index_14.C)
-SAMPLES_NAMES=(HI.0640.005.Index_14.C)
+SAMPLES_NAMES=(HI.0640.005.Index_13.B HI.0640.005.Index_14.C)
+
 
 #SAMPLE1=(HI.0635.008.Index_12.A_R1 HI.0640.005.Index_13.B_R1 HI.0640.005.Index_14.C_R1 HI.0640.005.Index_15.D_R1 HI.0640.005.Index_16.E_R1 HI.0640.006.Index_18.F_R1 HI.0640.006.Index_19.G_R1 HI.0640.006.Index_20.H_R1 HI.0640.006.Index_21.I_R1 )
 
@@ -99,42 +99,44 @@ run_subread_index(){
 
     docker run --platform linux/amd64 -it --rm -v $(pwd):/data nanozoo/subread:2.0.2--53f5da6 subread-buildindex -o /data/subread_B10_index/subread_index /data/referencyjny_genom_b10/pb_b10_ill1.fasta
 
-    #mv index.00.b.array index.00.b.tab index.files index.log index.reads subread_B10_index
-
-
-    #or i in "${!SAMPLE1[@]}"; do
-    #    docker run --platform linux/amd64 -it --rm -v $(pwd):/data nanozoo/subread:2.0.2--53f5da6 subread-align -T 3 -t 1 -d 50 -D 600 -i /data/subread_index/index -r /data/bfc_corrected_trimmomatic_"${SAMPLE1[i]}".fastq.gz -R /data/bfc_corrected_trimmomatic_"${SAMPLE2[i]}".fastq.gz -o /data/subread_"${SAMPLE1[i]}"_PE.bam
-    #done
-
-    #mv *.bam *.vcf subread_output/
 
 }
 
+
 run_hisat_index(){
  
-    mkdir hisat2_output
+    
     mkdir hisat2_index
 
     docker run --platform linux/amd64 -it --rm -v $(pwd):/data makaho/hisat2-zstd hisat2-build -p 20 /data/referencyjny_genom_b10/pb_b10_ill1.fasta /data/hisat2_index/hisat2_index
      
 }
 
+run_bowtie_index()(
+    mkdir bowtie_index
+    
+    docker run --platform linux/amd64 -it --rm -v $(pwd):/data alexeyebi/bowtie2_samtools bowtie2-build  /data/referencyjny_genom_b10/pb_b10_ill1.fasta /data/bowtie_index/bowtie_index
+
+
+)
+
 run_hisat_mapping_raw_files(){
-    #for i in "${!SAMPLE1[@]}"; do
-    #    mv bfc_corrected_trimmomatic_"${SAMPLE1[i]}".fastq.gz bfc_corrected_trimmomatic_"${SAMPLE1[i]}".fastq
-    #    mv bfc_corrected_trimmomatic_"${SAMPLE2[i]}".fastq.gz bfc_corrected_trimmomatic_"${SAMPLE2[i]}".fastq
-    #done
-
-
+ 
+    mkdir hisat2_output
     for i in "${!SAMPLE1[@]}"; do
         docker run --platform linux/amd64 -it --rm -v $(pwd):/data makaho/hisat2-zstd hisat2 -p 20 -x /data/hisat2_index/hisat2_index -1 /data/"${SAMPLE1[i]}".fastq.gz -2 /data/"${SAMPLE2[i]}".fastq.gz -S /data/hisat2_output/"${SAMPLES_NAMES[i]}".sam
     done
 
-#for i in "${!SAMPLE1[@]}"; do
-#    mv bfc_corrected_trimmomatic_"${SAMPLE1[i]}".fastq bfc_corrected_trimmomatic_"${SAMPLE1[i]}".fastq.gz
-#    mv bfc_corrected_trimmomatic_"${SAMPLE2[i]}".fastq bfc_corrected_trimmomatic_"${SAMPLE2[i]}".fastq.gz
-#done                             
 }
+
+run_bowtie_mapping_raw_files(){
+    mkdir bowtie2_output_raw_data_B10
+    for i in "${!SAMPLE1[@]}"; do
+        docker run --platform linux/amd64 -it --rm -v $(pwd):/data alexeyebi/bowtie2_samtools bowtie2 -p 20 -t -x /data/bowtie_index/bowtie_index -1 /data/"${SAMPLE1[i]}".fastq.gz -2 /data/"${SAMPLE2[i]}".fastq.gz -S /data/bowtie2_output_raw_data_B10/"${SAMPLES_NAMES[i]}".sam
+    done
+}
+
+
 
 hisat_sam_to_bam(){
     for sample in ${SAMPLES_NAMES}; do
@@ -170,6 +172,44 @@ hisat_sam_to_bam(){
 
 }
 
+sam_to_bam(){ # $1 = output folder of mapping
+    for sample in ${SAMPLES_NAMES}; do
+        docker run --platform linux/amd64 -it --rm -v $(pwd)/$1_output:/data staphb/samtools:1.13 samtools view -@ 20 -bS  /data/${sample}.sam  -o /data/${sample}.bam
+
+    done
+
+
+    for sample in ${SAMPLES_NAMES}; do
+        docker run --platform linux/amd64 -it --rm -v $(pwd)/$1_output:/data staphb/samtools:1.13 samtools sort -@ 20 /data/${sample}.bam  -o /data/${sample}_sorted.bam
+
+    done
+
+
+    for sample in ${SAMPLES_NAMES}; do
+        docker run --platform linux/amd64 -it --rm -v $(pwd)/$1_output:/data staphb/samtools:1.13 samtools index -@ 20 /data/${sample}_sorted.bam
+
+    done
+
+    for sample in ${SAMPLES_NAMES}; do
+        docker run --platform linux/amd64 -it --rm -v $(pwd)/$1_output:/data staphb/samtools:1.13 samtools idxstats -@ 20 /data/${sample}_sorted.bam > ${sample}_idxstats.txt
+
+    done
+
+    mv *.txt $1_output
+
+    for sample in ${SAMPLES_NAMES}; do
+        docker run --platform linux/amd64 -it --rm -v $(pwd)/$1_output:/data staphb/samtools:1.13 samtools stats -@ 20 /data/${sample}_sorted.bam > ${sample}_stats.txt
+
+    done
+
+    mv *.txt $1_output
+
+
+    
+}
+
+
+
 main(){
     #run_fastp
     #run_fastqc_on_fastp_data
@@ -186,7 +226,9 @@ main(){
  #########################   
     #run_hisat_index
     #run_hisat_mapping_raw_files
-    hisat_sam_to_bam
+    #hisat_sam_to_bam
+    #run_bowtie_index
+    run_bowtie_mapping_raw_files
 }
 main
 
